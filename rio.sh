60725eb4fd26efd204db0f02368fe7e6f6b3d82d
AndrewLester <alester220@gmail.com>
2020-01-05 22:29:14 -0500

    Add Spark Maxes to electrical test

--- rio.sh
+++ rio.sh
@@ -8,4 +8,4 @@ fi
 
 robotpy-installer $verb-robotpy
 robotpy-installer $verb-opkg $(< rio-opkgs.txt)
-robotpy-installer $verb-pip -r rio_requirements.txt
+robotpy-installer $verb-pip -r rio-requirements.txt
