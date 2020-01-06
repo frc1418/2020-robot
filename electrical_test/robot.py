e4dbcf9f50b1646ba2e4dc115348e39a7641dc19
AndrewLester <alester220@gmail.com>
2020-01-05 22:29:14 -0500

    Add Spark Maxes to electrical test

--- electrical_test/robot.py
+++ electrical_test/robot.py
@@ -3,26 +3,23 @@ import wpilib
 import wpilib.drive
 import math
 
-from ctre.wpi_talonsrx import WPI_TalonSRX
-
+from rev import CANSparkMax, MotorType
 
 class TestRobot(magicbot.MagicRobot):
-    ENCODER_PULSE_PER_REV = 1024
-    WHEEL_DIAMETER = 0.5
 
     def createObjects(self):
-        self.joystick_left = wpilib.Joystick(0)
-        self.joystick_right = wpilib.Joystick(1)
-        self.lf_motor = WPI_TalonSRX(10)
-        self.lr_motor = WPI_TalonSRX(15)
-        self.rf_motor = WPI_TalonSRX(20)
-        self.rr_motor = WPI_TalonSRX(25)
-        # Drivetrain
-        self.train = wpilib.drive.DifferentialDrive(wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor), wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor))
+
+        self.left_joystick = wpilib.Joystick(0)
+        self.right_joystick = wpilib.Joystick(1)
+        self.alt_joystick = wpilib.Joystick(2)
+
+        self.left_motors = wpilib.SpeedControllerGroup(CANSparkMax(10, MotorType.kBrushed), CANSparkMax(20, MotorType.kBrushed))
+        self.right_motors = wpilib.SpeedControllerGroup(CANSparkMax(30, MotorType.kBrushed), CANSparkMax(40, MotorType.kBrushed))
+
+        self.drive = wpilib.drive.DifferentialDrive(self.left_motors, self.right_motors)
 
     def teleopPeriodic(self):
-        self.train.arcadeDrive(-self.joystick_left.getY(), self.joystick_right.getX())
-
+        self.drive.arcadeDrive(-self.left_joystick.getY(), self.left_joystick.getX())
 
 if __name__ == '__main__':
     wpilib.run(TestRobot)
