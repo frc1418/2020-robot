8ad6d46df571770322223ee226171f145c69449a
Brandon Werbel <brandon@werbel.org>
2020-01-11 13:47:33 -0500

    Implements color sensor to find closest color

--- electrical_test/ColorMatch.py
+++ electrical_test/ColorMatch.py
@@ -4,11 +4,9 @@ from ColorSensorV3 import ColorSensorV3
 #TODO import util
 #import edu.wpi.first.wpilibj.util.Color
 #import edu.wpi.first.wpilibj.util.ColorShim
-class Color:
-    pass
 
 class ColorMatch:
-    def CalculateDistance(self, color1: Color, color2: Color):
+    def CalculateDistance(self, color1: ColorSensorV3.RawColor, color2: ColorSensorV3.RawColor):
         redDiff = color1.red - color2.red
         greenDiff = color1.green - color2.green
         blueDiff = color1.blue - color2.blue
@@ -21,7 +19,7 @@ class ColorMatch:
     def __init__(self):
         self.confidenceLevel = self.kDefaultConfidence
 
-    def addColorMatch(self, color: Color):
+    def addColorMatch(self, color: ColorSensorV3.RawColor):
         self.colorsToMatch.append(color)
 
     def setConfidenceThreshold(self, confidence: float):
@@ -32,7 +30,7 @@ class ColorMatch:
         
         self.confidenceLevel = confidence
 
-    def matchColor(self, colorToMatch: Color):
+    def matchColor(self, colorToMatch: ColorSensorV3.RawColor):
         match = self.matchClosestColor(colorToMatch)
         if match.confidence > self.confidenceLevel:
             return match
@@ -40,13 +38,14 @@ class ColorMatch:
         return None
 
     def makeColor(self, r: float, g: float, b: float):
-        return Color(r, g, b)
+        return ColorSensorV3.RawColor(r, g, b, 0)
 
-    def matchClosestColor(self, color: Color):
+    def matchClosestColor(self, color: ColorSensorV3.RawColor):
         magnitude = color.red + color.blue + color.green
 
         if magnitude > 0.0 and len(self.colorsToMatch) > 0:
-            normalized = Color(color.red / magnitude, color.green / magnitude, color.blue / magnitude)
+            normalized = ColorSensorV3.RawColor(
+                color.red / magnitude, color.green / magnitude, color.blue / magnitude, 0)
             minDistance = 1.0
             idx = 0
 
@@ -66,6 +65,6 @@ class ColorMatch:
 
 
 class ColorMatchResult:
-    def __init__(self, color: Color, confidence: float):
+    def __init__(self, color: ColorSensorV3.RawColor, confidence: float):
         self.color = color
         self.confidence = confidence
--- electrical_test/robot.py
+++ electrical_test/robot.py
@@ -2,6 +2,7 @@ import magicbot
 import wpilib
 import wpilib.drive
 import math
+import time
 from magicbot import tunable
 
 from rev import CANSparkMax, MotorType
@@ -11,14 +12,15 @@ from ColorMatch import ColorMatch
 
 class TestRobot(magicbot.MagicRobot):
 
-    
+    red = tunable(0, writeDefault=False)
+    green = tunable(0, writeDefault=False)
+    blue = tunable(0, writeDefault=False)
     colorString = tunable("", writeDefault=False)
-    colorMatcher = ColorMatch()
 
-    BlueTarget = ColorSensorV3.RawColor(0, 255, 255, 0)
-    GreenTarget = ColorSensorV3.RawColor(0, 255, 0, 0)
-    RedTarget = ColorSensorV3.RawColor(255, 0, 0, 0)
-    YellowTarget = ColorSensorV3.RawColor(0, 255, 255, 0)
+    BlueTarget = ColorSensorV3.RawColor(0.187, 0.457, 0.376, 0) # accurate from ~16.2 cm
+    GreenTarget = ColorSensorV3.RawColor(0.178, 0.573, 0.252, 0) # accurate from ~12.7 cm
+    RedTarget = ColorSensorV3.RawColor(0.497, 0.365, 0.143, 0) # accurate from ~12.2 cm
+    YellowTarget = ColorSensorV3.RawColor(0.317, 0.557, 0.124, 0) # default color
 
     def createObjects(self):
         # self.left_joystick = wpilib.Joystick(0)
@@ -29,27 +31,37 @@ class TestRobot(magicbot.MagicRobot):
         # self.right_motors = wpilib.SpeedControllerGroup(CANSparkMax(30, MotorType.kBrushed), CANSparkMax(40, MotorType.kBrushed))
 
         # self.drive = wpilib.drive.DifferentialDrive(self.left_motors, self.right_motors)
-
+        self.last_time = time.time()
         self.colorSensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
+
+        self.colorMatcher = ColorMatch()
 
         self.colorMatcher.addColorMatch(self.BlueTarget)
         self.colorMatcher.addColorMatch(self.GreenTarget)
         self.colorMatcher.addColorMatch(self.RedTarget)
         self.colorMatcher.addColorMatch(self.YellowTarget)
-    def teleopPeriodic(self):
-        detectedColor = self.colorSensor.getColor()
 
-        match = self.colorMatcher.matchClosestColor(detectedColor)
-        if match.color == self.BlueTarget:
-            self.colorString = "Blue"
-        elif match.color == self.RedTarget:
-            self.colorString = "Red"
-        elif match.color == self.GreenTarget:
-            self.colorString = "Green"
-        elif match.color == self.YellowTarget:
-            self.colorString = "Yellow"
+    def teleopPeriodic(self):
+        try:
+            detectedColor = self.colorSensor.getColor()
+        except IOError:
+            pass
         else:
-            self.colorString = "Unknown"
+            self.red = detectedColor.red
+            self.blue = detectedColor.blue
+            self.green = detectedColor.green
+
+            match = self.colorMatcher.matchClosestColor(detectedColor)
+            if match.color == self.BlueTarget:
+                self.colorString = "Blue"
+            elif match.color == self.RedTarget:
+                self.colorString = "Red"
+            elif match.color == self.GreenTarget:
+                self.colorString = "Green"
+            elif match.color == self.YellowTarget:
+                self.colorString = "Yellow"
+            else:
+                self.colorString = "Unknown"
 
 if __name__ == '__main__':
     wpilib.run(TestRobot)
