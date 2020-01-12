16bed004da2c42437d6898cda747df00d136fa3c
BobbyMi11er <bobby@tybeemillers.com>
2020-01-12 14:54:19 -0500

    update drive.py

--- robot/components/drive.py
+++ robot/components/drive.py
@@ -18,35 +18,25 @@ class Drive:
     rotational_constant = tunable(0.5)
     squared_inputs = tunable(False)
 
-    fine_movement = will_reset_to(False)
-    fine_speed_multiplier = tunable(0.5)
-    fine_rotation_multiplier = tunable(0.5)
-
     def __init__(self):
-        self.enabled = False
+        pass
 
     def setup(self):
-        """
-        Set input threshold.
-        """
-        self.train.setDeadband(0.1)
+        pass
 
-    def move(self, y: float, rot: float, fine_movement: bool = False):
+    def move(self, y: float, rot: float):
         """
         Move robot.
         :param y: Speed of motion in the y direction. [-1..1]
         :param rot: Speed of rotation. [-1..1]
-        :param fine_movement: Decrease speeds for precise motion.
         """
         self.y = y
         self.rot = rot
-        self.fine_movement = fine_movement
 
     def execute(self):
         """
         Handle driving.
         """
-        self.train.arcadeDrive(self.speed_constant * self.y * (self.fine_speed_multiplier if self.fine_movement else 1),
-                               self.rotational_constant * self.rot * (self.fine_rotation_multiplier if self.fine_movement else 1),
+        self.train.arcadeDrive(self.speed_constant * self.y,
+                               self.rotational_constant * self.rot,
                                squaredInputs=self.squared_inputs)
-        
