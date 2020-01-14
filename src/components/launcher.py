eca8d49c1d1582c975077bc0184c0173aba3c3c0
BobbyMi11er <bobby@tybeemillers.com>
2020-01-14 17:20:37 -0500

    use encoder velocity

--- src/components/launcher.py
+++ src/components/launcher.py
@@ -5,12 +5,18 @@ class Launcher:
 
     def setup(self):
         self.PID_Controller = self.launcher_motor.getPIDController()
+        self.encoder = self.launcher_motor.getEncoder()
 
     def warmUp(self):
+        self.warm_Up = False
         self.launcher_motor.set(1)
         if (self.launcher_motor.getVoltage() > 8):
-            self.readyToFire = True
+            self.warm_Up = True
 
     def execute(self):
-        if (self.readyToFire == True):
+        if (self.warm_Up == True):
             self.PID_Controller.setReference(1000, ControlType.kVelocity, pidSlot=0, arbFeedforward=0)
+            if (self.encoder.getVelocity() > 990):
+                self.readyToFire = True
+                 
+            
