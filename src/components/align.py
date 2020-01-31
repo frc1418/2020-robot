5f38288c6ecd6e3d2997a2b862f13cc946e7b58f
BobbyMi11er <bobby@tybeemillers.com>
2020-01-31 16:15:15 -0500

    Add align component

---
+++ src/components/align.py
@@ -0,0 +1,11 @@
+from drive import Drive
+from common.limelight import Limelight
+
+class Align():
+    def createObjects(self):
+        self.limelight = Limelight()
+        self.drive = Drive()
+    
+    def execute(self):
+        angle = limelight.getYaw()
+        self.drive.align(angle, True)
