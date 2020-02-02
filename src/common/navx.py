5f83cd4dd4c8077051035383fa7fdacd58740e13
AndrewLester <alester220@gmail.com>
2020-02-01 22:42:08 -0500

    Use import safety on navx

---
+++ src/common/navx.py
@@ -0,0 +1,14 @@
+try:
+    import navx
+except ImportError:
+    class AHRS:
+        def create_spi(self):
+            return None
+
+    class navx:
+        AHRS = AHRS()
+        def getAngle(self):
+            return 5
+
+        def reset(self):
+            return None
