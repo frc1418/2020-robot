4dae9c790fadbf006fb6a35818f78b7014585078
AndrewLester <alester220@gmail.com>
2020-02-04 16:23:43 -0500

    Use correct encoders

--- characterization/robot.py
+++ characterization/robot.py
@@ -4,33 +4,46 @@ import magicbot
 import navx
 import wpilib
 from wpilib.drive import DifferentialDrive
-from ctre import WPI_TalonSRX
-from networktables.networktables import NetworkTables, NetworkTablesInstance
+from networktables import NetworkTables, NetworkTablesInstance
 from networktables.util import ntproperty
-from magicbot import tunable
+from rev import CANSparkMax, MotorType
 
 
-class Robot(magicbot.MagicRobot):
+class Robot(wpilib.IterativeRobot):
     WHEEL_DIAMETER = 0.1524  # Units: Meters
-    ENCODER_PULSE_PER_REV = 1024
+    ENCODER_PULSE_PER_REV = 42
+    GEARING = 7.56  # 6:1 gear ratio
 
     autoSpeedEntry = ntproperty('/robot/autospeed', 0.0)
     telemetry_entry = ntproperty('/robot/telemetry', [0.0], writeDefault=False)
     rotateEntry = ntproperty('/robot/rotate', False)
 
-    l_encoder_pos = tunable(0)
-    l_encoder_rate = tunable(0)
-    r_encoder_pos = tunable(0)
-    r_encoder_rate = tunable(0)
+    l_encoder_pos = ntproperty('/l_encoder_pos', 0)
+    l_encoder_rate = ntproperty('/l_encoder_rate', 0)
+    r_encoder_pos = ntproperty('/r_encoder_pos', 0)
+    r_encoder_rate = ntproperty('/r_encoder_rate', 0)
 
 
-    def createObjects(self):
+    def robotInit(self):
         self.prior_autospeed = 0
 
         self.joystick = wpilib.Joystick(0)
 
-        self.left_motors = wpilib.SpeedControllerGroup(WPI_TalonSRX(10), WPI_TalonSRX(30))
-        self.right_motors = wpilib.SpeedControllerGroup(WPI_TalonSRX(20), WPI_TalonSRX(40))
+        self.left_motor_master = CANSparkMax(1, MotorType.kBrushless)
+        self.right_motor_master = CANSparkMax(4, MotorType.kBrushless)
+
+        # Set up Speed Controller Groups
+        self.left_motors = wpilib.SpeedControllerGroup(
+            self.left_motor_master,
+            CANSparkMax(2, MotorType.kBrushless),
+            CANSparkMax(3, MotorType.kBrushless)
+        )
+
+        self.right_motors = wpilib.SpeedControllerGroup(
+            self.right_motor_master,
+            CANSparkMax(5, MotorType.kBrushless),
+            CANSparkMax(6, MotorType.kBrushless)
+        )
 
         # Configure Gyro
 
@@ -45,15 +58,18 @@ class Robot(magicbot.MagicRobot):
 
         # Configure encoder related functions -- getDistance and getrate should return
         # units and units/s
+        self.encoder_constant = (1 / self.GEARING) * self.WHEEL_DIAMETER * math.pi
 
-        self.encoder_constant = (1 / self.ENCODER_PULSE_PER_REV) * self.WHEEL_DIAMETER * math.pi
+        self.left_encoder = self.left_motor_master.getEncoder()
+        self.left_encoder.setPositionConversionFactor(self.encoder_constant)
+        self.left_encoder.setVelocityConversionFactor(self.encoder_constant / 60)
 
-        self.left_encoder = wpilib.Encoder(0, 1)
-        self.left_encoder.setDistancePerPulse(self.encoder_constant)
+        self.right_encoder = self.right_motor_master.getEncoder()
+        self.right_encoder.setPositionConversionFactor(self.encoder_constant)
+        self.right_encoder.setVelocityConversionFactor(self.encoder_constant / 60)
 
-        self.right_encoder = wpilib.Encoder(2, 3)
-        self.right_encoder.setReverseDirection(True)
-        self.right_encoder.setDistancePerPulse(self.encoder_constant)
+        self.left_encoder.setPosition(0);
+        self.right_encoder.setPosition(0);
 
         # Set the update rate instead of using flush because of a ntcore bug
         # -> probably don't want to do this on a robot in competition
@@ -65,16 +81,17 @@ class Robot(magicbot.MagicRobot):
 
     def robotPeriodic(self):
         # feedback for users, but not used by the control program
-        self.l_encoder_pos = self.left_encoder.get()
-        self.l_encoder_rate = self.left_encoder.getRate()
-        self.r_encoder_pos = self.right_encoder.get()
-        self.r_encoder_rate = self.right_encoder.getRate()
+        self.l_encoder_pos = self.left_encoder.getPosition()
+        self.l_encoder_rate = self.left_encoder.getVelocity()
+        self.r_encoder_pos = self.right_encoder.getPosition()
+        self.r_encoder_rate = self.right_encoder.getVelocity()
 
     def teleopInit(self):
         print('Robot in operator control mode')
 
     def teleopPeriodic(self):
         self.drive.arcadeDrive(-self.joystick.getY(), self.joystick.getX())
+        print(f'Left Distance: {self.left_encoder.getPosition()}')
 
     def autonomousInit(self):
         print('Robot in autonomous mode')
@@ -87,16 +104,17 @@ class Robot(magicbot.MagicRobot):
     # setUpdateRate call.
 
     def autonomousPeriodic(self):
-        # Retrieve values to send back before telling the motors to do something
+        # Retrieve values to send back before telling the motors to do somethin
+
         now = wpilib.Timer.getFPGATimestamp()
 
-        leftPosition = self.left_encoder.getDistance()
-        leftRate = self.left_encoder.getRate()
+        leftPosition = self.left_encoder.getPosition()
+        leftRate = self.left_encoder.getVelocity()
 
-        rightPosition = self.right_encoder.getDistance()
-        rightRate = self.right_encoder.getRate()
+        rightPosition = self.right_encoder.getPosition()
+        rightRate = self.right_encoder.getVelocity()
 
-        battery = wpilib.RobotController.getBatteryVoltage()
+        battery = wpilib.RobotController.getInputVoltage()
         motorVolts = battery * abs(self.prior_autospeed)
 
         leftMotorVolts = motorVolts
