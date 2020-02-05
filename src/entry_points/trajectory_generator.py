2cda266d68bf2238b81a2e1aef7f04ccfb867717
BobbyMi11er <bobby@tybeemillers.com>
2020-02-05 17:35:03 -0500

    Use Trajectory Generation Libraries

--- src/entry_points/trajectory_generator.py
+++ src/entry_points/trajectory_generator.py
@@ -4,6 +4,8 @@ import pickle
 import pathfinder as pf
 from . import robotpy_entry_point
 import math
+from wpilib.geometry import Pose2d, Rotation2d, Translation2d
+from wpilib.trajectory import TrajectoryGenerator, TrajectoryConfig
 
 
 WHEELBASE_WIDTH = 1.83  # Units: ft
@@ -75,28 +77,50 @@ def generate_trajectories(options, robot_class):
     :param options: Options for the entry point being called
     :param robot_class: The class of the robot being run
     """
-    print('Generating Trajectories...')
-    generated_trajectories = {}
+    # print('Generating Trajectories...')
+    # generated_trajectories = {}
 
-    for trajectory_name, trajectory in trajectories.items():
-        print(f'Trajectory: {trajectory_name} ...', end='')
-        generated_trajectory = pf.generate(
-            trajectory,
-            pf.FIT_HERMITE_CUBIC,
-            pf.SAMPLES_HIGH,
-            dt=0.02,  # 20ms
-            max_velocity=MAX_GENERATION_VELOCITY,  # These are in ft/sec and
-            max_acceleration=MAX_GENERATION_ACCELERATION,  # set the units for distance to ft.
-            max_jerk=MAX_GENERATION_JERK
-        )[1]  # The 0th element is just info
+    # for trajectory_name, trajectory in trajectories.items():
+    #     print(f'Trajectory: {trajectory_name} ...', end='')
+    #     generated_trajectory = pf.generate(
+    #         trajectory,
+    #         pf.FIT_HERMITE_CUBIC,
+    #         pf.SAMPLES_HIGH,
+    #         dt=0.02,  # 20ms
+    #         max_velocity=MAX_GENERATION_VELOCITY,  # These are in ft/sec and
+    #         max_acceleration=MAX_GENERATION_ACCELERATION,  # set the units for distance to ft.
+    #         max_jerk=MAX_GENERATION_JERK
+    #     )[1]  # The 0th element is just info
 
-        modifier = pf.modifiers.TankModifier(generated_trajectory).modify(WHEELBASE_WIDTH)
+    #     modifier = pf.modifiers.TankModifier(generated_trajectory).modify(WHEELBASE_WIDTH)
 
-        generated_trajectories[trajectory_name] = (
-            modifier.getLeftTrajectory(),
-            modifier.getRightTrajectory()
-        )
-        print('Done')
+    #     generated_trajectories[trajectory_name] = (
+    #         modifier.getLeftTrajectory(),
+    #         modifier.getRightTrajectory()
+    #     )
+    #     print('Done')
 
-    _write_trajectories(generated_trajectories)
-    print('Finished.')
+    # _write_trajectories(generated_trajectories)
+    # print('Finished.')
+    # UNITS: Meters
+    start = Pose2d(0, 0, Rotation2d.fromDegrees(0))
+    finish = Pose2d(0, 1, Rotation2d.fromDegrees(0))
+
+    # 2018 cross scale auto waypoints
+    #   const frc::Pose2d sideStart{1.54_ft, 23.23_ft, frc::Rotation2d(180_deg)};
+    #   const frc::Pose2d crossScale{23.7_ft, 6.8_ft, frc::Rotation2d(-160_deg)};
+
+    interiorWaypoints = []
+
+    #   std::vector<frc::Translation2d> interiorWaypoints{
+    #       frc::Translation2d{14.54_ft, 23.23_ft},
+    #       frc::Translation2d{21.04_ft, 18.23_ft}};
+
+    config = TrajectoryConfig(12, 12)
+    config.setReversed(False)
+
+    trajectory = TrajectoryGenerator.generateTrajectory(
+        start, interiorWaypoints, finish, config
+    )
+    return trajectory
+
