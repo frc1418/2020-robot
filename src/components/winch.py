369e9ed993ccc5eec21b07f5a933fe4d07268acf
quirozj <quirozj@student.fccps.org>
2020-02-05 08:18:11 -0500

    Fix format errors.

--- src/components/winch.py
+++ src/components/winch.py
@@ -3,41 +3,31 @@ from common.rev import CANSparkMax
 from magicbot import will_reset_to
 from magicbot import tunable
 
+
 class Climber:
-   climber_motor: CANSparkMax
-   climber_solenoid: wpilib.DoubleSolenoid
-   climber_on = will_reset_to(False)
-   climber_piston = will_reset_to(True)
-   climber_traveled = will_reset_to(0)
-   #ENCODER_TICKS_PER_REVOLUTION = 55000
+    climber_motor: CANSparkMax
+    climber_solenoid: wpilib.DoubleSolenoid
+    climber_on = will_reset_to(False)
+    climber_piston = will_reset_to(True)
+    climber_traveled = will_reset_to(0)
+    # ENCODER_TICKS_PER_REVOLUTION = 55000
 
     def raise_scissor(self):
-       self.climber_piston = False
+        self.climber_piston = False
 
     def lower_scissor(self):
-       self.climber_piston = True
-    
+        self.climber_piston = True
+
     def winch_motor_on(self):
         self.climber_piston = True
-    
+
     def execute(self):
         if self.climber_piston:
             self.climber_solenoid = wpilib.DoubleSolenoid.Value.kForward
         else:
             self.climber_solenoid = wpilib.DoubleSolenoid.Value.kReverse
-        
+
         if self.climber_on:
             self.climber_motor.set(1)
         else:
             self.climber_motor.set(0)
-
-    
-
-
-
-
-# need to do:
-# - fire piston
-# - unfire piston (to drop scissor)
-# - turn on winch motor
-# - turn off winch motor after motor goes certain distance/encoder goes certain num of rotations
