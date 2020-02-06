aa61d5d18339f2a4040761f18f2cf7f1d89a3021
AndrewLester <alester220@gmail.com>
2020-02-06 15:13:00 -0500

    Add follower_state decorator

---
+++ src/autonomous/__init__.py
@@ -0,0 +1,20 @@
+from magicbot import state
+from functools import partial, wraps
+
+
+def follower_state(f=None, *, trajectory_name: str, follower, next_state=None, first=False, must_finish=False):
+    """
+    Denotes a state that follows a trajectory and stops when it finishes
+    """
+    if f is None:
+        return partial(follower_state, trajectory_name=trajectory_name, first=first, must_finish=must_finish)
+
+    @wraps(f)
+    def wrapper(self, initial_call, tm, state_tm):
+        # done = self.follower.follow_trajectory(trajectory_name, state_tm)
+        f(self, initial_call, tm, state_tm)
+        # if done:
+            # self.next_state(next_state)
+
+    return state(wrapper, first=first, must_finish=must_finish)
+
