5db012ff60da51d7ed8da9e168c9a41d6e947a3a
AndrewLester <alester220@gmail.com>
2020-02-06 22:06:36 -0500

    Use passed in follower object for decorator

--- src/autonomous/__init__.py
+++ src/autonomous/__init__.py
@@ -7,11 +7,11 @@ def follower_state(f=None, *, trajectory_name: str, follower, next_state=None, f
     Denotes a state that follows a trajectory and stops when it finishes
     """
     if f is None:
-        return partial(follower_state, trajectory_name=trajectory_name, first=first, must_finish=must_finish)
+        return partial(follower_state, trajectory_name=trajectory_name, follower=follower, first=first, must_finish=must_finish)
 
     @wraps(f)
     def wrapper(self, initial_call, tm, state_tm):
-        # done = self.follower.follow_trajectory(trajectory_name, state_tm)
+        # done = follower.follow_trajectory(trajectory_name, state_tm)
         f(self, initial_call, tm, state_tm)
         # if done:
             # self.next_state(next_state)
