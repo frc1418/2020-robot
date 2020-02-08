d0fe2906b351b3d6e3eff97def821106afb1a7de
BobbyMi11er <bobby@tybeemillers.com>
2020-02-08 11:46:22 -0500

    Add to odometry.update

--- src/components/odometry.py
+++ src/components/odometry.py
@@ -1,6 +1,7 @@
 from wpilib.kinematics import DifferentialDriveKinematics, DifferentialDriveOdometry
 from wpilib.geometry import Rotation2d
 from common.rev import CANEncoder
+from networktables.util import ntproperty
 import math
 import navx
 
@@ -10,6 +11,8 @@ class Odometry:
     navx: navx.AHRS
     left_encoder: CANEncoder
     right_encoder: CANEncoder
+    left_distance = ntproperty('/left distance/', 0)
+    right_distance = ntproperty('/right distance/', 0)
 
     def getAngle(self):
         """Return the navx angle with counter-clockwise as positive"""
@@ -17,19 +20,17 @@ class Odometry:
 
     def setup(self):
         self.odometry = DifferentialDriveOdometry(Rotation2d(math.radians(self.getAngle())))
-        self.left_distance = 0
-        self.right_distance = 0
 
     def get_pose(self):
         self.odometry.getPose()
 
-    def get_distance(self, left = True):
+    def get_distance(self, left=True):
         if left:
             return self.left_distance
         else:
             return self.right_distance
 
     def execute(self):
-        self.odometry.update(Rotation2d(math.radians(self.getAngle())), 0, 0)
+        self.odometry.update(Rotation2d(math.radians(self.getAngle())), self.left_encoder.getPosition(), self.right_encoder.getPosition)
         self.left_distance = self.left_encoder.getPosition()
         self.right_distance = self.right_encoder.getPosition()
