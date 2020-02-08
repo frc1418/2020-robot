7335461771a0649ff3ce4dbbba8c54f7981518bc
Brandon Werbel <brandon@werbel.org>
2020-02-08 15:05:21 -0500

    Create different ntproperty for position and rotation control

--- src/Controllers/panelSpinner.py
+++ src/Controllers/panelSpinner.py
@@ -9,11 +9,16 @@ class PanelSpinner(StateMachine):
 
     control_panel: ControlPanel
 
-    isSpinning = ntproperty('/Controllers/panelSpinner/isSpinning', False)
+    isSpinningPosition = ntproperty('/Controllers/panelSpinner/isSpinningPosition', False)
+    isSpinningRotation = ntproperty('/Controllers/panelSpinner/isSpinningRotation', False)
 
     def spin_to(self, position=False):
-        self.engage('positionControl' if position else 'rotationControl')
-        self.isSpinning = True
+        if position:
+            self.engage('positionControl')
+            self.isSpinningPosition = True
+        else:
+            self.engage('rotationControl')
+            self.isSpinningRotation = True
 
     # First here doesn't matter because we use the argument form of engage
     @state(first=True, must_finish=True)
@@ -27,11 +32,13 @@ class PanelSpinner(StateMachine):
             self.last_color = self.control_panel.detected_color
         self.control_panel.spin(0.25)
         if self.rotations >= 18:
-            self.isSpinning = False
+            self.isSpinningRotation = False
             self.done()
 
     @state(must_finish=True)
-    def positionControl(self):
+    def positionControl(self, state_tm):
+        print(state_tm)
+
         if self.control_panel.turn_to_color is None or self.control_panel.detected_color is None:
             return
 
@@ -40,7 +47,14 @@ class PanelSpinner(StateMachine):
             direction = math.copysign(1, self.control_panel.colors.index(
                 self.control_panel.detected_color) - self.control_panel.colors.index(self.control_panel.turn_to_color))
             self.control_panel.spin(direction * 0.17)
+            self.resting_timestamp = None
         else:
-            self.control_panel.spin(0)
-            self.isSpinning = False
-            self.done()
+            if self.resting_timestamp is None:
+                self.resting_timestamp = state_tm
+
+            # Wait on the correct color for one second
+            if state_tm - self.resting_timestamp < 1:
+                self.control_panel.spin(0)
+            else:
+                self.done()
+                self.isSpinningPosition = True
