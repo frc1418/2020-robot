e2a92ceefde9a9cdcc5dd54009153213e979401e
Brandon Werbel <brandon@werbel.org>
2020-02-08 11:26:46 -0500

    Use spin() instead of cp_motor.set() to spin motor

--- src/common/panelSpinner.py
+++ src/common/panelSpinner.py
@@ -21,7 +21,7 @@ class PanelSpinner(StateMachine):
         if self.control_panel.detected_color != self.last_color:
             self.rotations += 1
             self.last_color = self.control_panel.detected_color
-        self.control_panel.cp_motor.set(0.25)
+        self.control_panel.spin(0.25)
         if self.rotations >= 18:
             self.done()
 
@@ -34,7 +34,7 @@ class PanelSpinner(StateMachine):
             # print(f'Detected: {self.detected_color.red}, {self.detected_color.green}, {self.detected_color.blue}  Towards: {self.turn_to_color.red}, {self.turn_to_color.green}, {self.turn_to_color.blue}')
             direction = math.copysign(1, self.control_panel.colors.index(
                 self.control_panel.detected_color) - self.control_panel.colors.index(self.control_panel.turn_to_color))
-            self.control_panel.cp_motor.set(direction * 0.17)
+            self.control_panel.spin(direction * 0.17)
         else:
-            self.control_panel.cp_motor.set(0)
+            self.control_panel.spin(0)
             self.done()
