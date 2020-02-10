2ae613b3cb6a4311a95f6d72827b8a4ed14fd260
BobbyMi11er <bobby@tybeemillers.com>
2020-02-10 15:41:12 -0500

    Use trajectory state curvature

--- src/autonomous/initiation.py
+++ src/autonomous/initiation.py
@@ -18,27 +18,28 @@ class Initiation(AutonomousStateMachine):
     launcher_motors: wpilib.SpeedControllerGroup
     launcher_solenoid: wpilib.Solenoid
 
-    # def on_enable(self):
-    #     self.shot_count = 0
+    def on_enable(self):
+        super().on_enable()
+        self.shot_count = 0
 
-    # @default_state
-    # def spinup(self, state_tm):
-    #     if self.shot_count >= 3:
-    #         self.done()
+    @default_state
+    def spinup(self, state_tm):
+        if self.shot_count >= 3:
+            self.done()
 
-    #     # TODO: Use RPM to check if spun up
-    #     self.launcher_motors.set(0.75)
-    #     if state_tm > 5 or (state_tm > 1 and self.shot_count > 0):
-    #         self.next_state('shoot')
+        # TODO: Use RPM to check if spun up
+        self.launcher_motors.set(0.75)
+        if state_tm > 5 or (state_tm > 1 and self.shot_count > 0):
+            self.next_state('shoot')
 
-    # @timed_state(duration=1, first=False)
-    # def shoot(self, state_tm):
-    #     self.launcher_motors.set(0.75)  # Continue to set launcher motor speed
-    #     self.launcher_solenoid.set(True)
-    #     if state_tm > 0.5:
-    #         self.launcher_solenoid.set(False)
-    #     self.shot_count += 1
+    @timed_state(duration=1, first=False)
+    def shoot(self, state_tm):
+        self.launcher_motors.set(0.75)  # Continue to set launcher motor speed
+        self.launcher_solenoid.set(True)
+        if state_tm > 0.5:
+            self.launcher_solenoid.set(False)
+        self.shot_count += 1
 
-    @state(first=True)
+    @follower_state(trajectory_name='charge', first=True)
     def moveForward(self, state_tm):
-        self.follower.follow_trajectory("charge", state_tm)
+        pass
