1eb9832d429f6b5dcc419d524fc705a71402791c
AndrewLester <alester220@gmail.com>
2020-02-13 15:46:36 -0500

    Make is_finished method trajectory specific

--- src/autonomous/__init__.py
+++ src/autonomous/__init__.py
@@ -16,7 +16,7 @@ def follower_state(f=None, *, trajectory_name: str, next_state=None, first=False
     def wrapper(self, initial_call, tm, state_tm):
         self.follower.follow_trajectory(trajectory_name, state_tm)
         wrapped_state.run(tm, state_tm, initial_call)
-        if self.follower.is_finished():
+        if self.follower.is_finished(trajectory_name):
             if next_state is not None:
                 self.next_state(next_state)
 
