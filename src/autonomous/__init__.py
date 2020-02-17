b069182d378d3220f9c1ea379fd14f5971e0770e
Andrew Lester <alester220@gmail.com>
2020-02-16 20:28:29 -0500

    Pass self into wrapped state call in follower_state
    Closes #105

--- src/autonomous/__init__.py
+++ src/autonomous/__init__.py
@@ -15,7 +15,7 @@ def follower_state(f=None, *, trajectory_name: str, next_state=None, first=False
     @wraps(f)
     def wrapper(self, initial_call, tm, state_tm):
         self.follower.follow_trajectory(trajectory_name, state_tm)
-        wrapped_state.run(tm, state_tm, initial_call)
+        wrapped_state.run(self, tm, state_tm, initial_call)
         if self.follower.is_finished(trajectory_name):
             if next_state is not None:
                 self.next_state(next_state)
