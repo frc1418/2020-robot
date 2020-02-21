30ab231c41f782e37db8de3a8d874a2908ba96b7
AndrewLester <alester220@gmail.com>
2020-02-21 11:38:39 -0500

    Remove wraps in follower state

--- src/autonomous/__init__.py
+++ src/autonomous/__init__.py
@@ -1,6 +1,6 @@
 from magicbot import state
 from magicbot.state_machine import _create_wrapper
-from functools import partial, wraps
+from functools import partial
 
 
 def follower_state(f=None, *, trajectory_name: str, next_state=None, first=False, must_finish=False):
@@ -10,7 +10,6 @@ def follower_state(f=None, *, trajectory_name: str, next_state=None, first=False
     if f is None:
         return partial(follower_state, trajectory_name=trajectory_name, first=first, must_finish=must_finish)
 
-    @wraps(f)
     def wrapper(self, initial_call, tm, state_tm):
         self.follower.follow_trajectory(trajectory_name, state_tm)
         f(self, tm, state_tm, initial_call)
