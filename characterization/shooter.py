4f9e6c1757c50b9bd351556be08f1a643205d22e
AndrewLester <alester220@gmail.com>
2020-03-05 18:03:28 -0500

    Use PID controller on the spark max

---
+++ characterization/shooter.py
@@ -0,0 +1,87 @@
+
+import wpilib
+from networktables.util import ntproperty
+from networktables import NetworkTablesInstance
+import math
+# Add VictorSPX try import
+try:
+    from ctre import WPI_VictorSPX
+except ImportError:
+    WPI_VictorSPX = type('WPI_VictorSPX', (wpilib.interfaces.SpeedController,), {})
+
+class Robot(wpilib.TimedRobot):
+
+    ENCODER_PULSES_PER_REV = 2048  # Ignore quadrature decoding (4x)
+    GEARING = 1  # 3.25:1 in gearbox
+
+    encoder_pos = ntproperty('/robot/encoder_pos', 0)
+    encoder_rate = ntproperty('/robot/encoder_rate', 0)
+
+    autoSpeedEntry = ntproperty('/robot/autospeed', 0, writeDefault=False)
+    telemetryEntry = ntproperty('/robot/telemetry', [0, 0, 0, 0, 0, 0], writeDefault=False)
+
+    def robotInit(self):
+        self.joystick = wpilib.Joystick(0)
+
+        self.motors = wpilib.SpeedControllerGroup(WPI_VictorSPX(2), WPI_VictorSPX(3))
+
+        self.priorAutospeed = 0
+        # TODO: Check if we need IdleMode.kBrake
+        # self.motor.setIdleMode(IdleMode.kBrake);
+
+        self.encoder = wpilib.Encoder(8, 7, True, encodingType=wpilib.Encoder.EncodingType.k1X)
+
+        # //
+        # // Configure encoder related functions -- getDistance and getrate should
+        # // return units and units/s
+        # //
+
+        # % if units == 'Degrees':
+        # double encoderConstant = (1 / GEARING) * 360
+        # % elif units == 'Radians':
+        # double encoderConstant = (1 / GEARING) * 2. * Math.PI;
+        # % elif units == 'Rotations':
+        self.encoderConstant = (1 / (self.ENCODER_PULSES_PER_REV))
+        self.encoder.setDistancePerPulse(self.encoderConstant)
+
+        self.encoder.reset()
+
+        NetworkTablesInstance.getDefault().setUpdateRate(0.010)
+
+    def disabledInit(self):
+        self.motors.set(0)
+
+    def robotPeriodic(self):
+        self.encoder_pos = self.encoder.getDistance()
+        self.encoder_rate = self.encoder.getRate()
+
+    def teleopPeriodic(self):
+        self.motors.set(-self.joystick.getY())
+        print(self.encoder.getDistance())
+        print(f'Rate: {self.encoder.getRate()}')
+
+    def autonomousPeriodic(self):
+
+        # // Retrieve values to send back before telling the motors to do something
+        now = wpilib.Timer.getFPGATimestamp()
+
+        position = self.encoder.getDistance()
+        rate = self.encoder.getRate()
+
+        battery = wpilib.RobotController.getInputVoltage()
+
+        motorVolts = battery * abs(self.priorAutospeed)
+
+        # // Retrieve the commanded speed from NetworkTables
+        autospeed = self.autoSpeedEntry
+        self.priorAutospeed = autospeed
+
+        self.motors.set(autospeed)
+
+        self.telemetryEntry = [
+            now, battery, autospeed,
+            motorVolts, position, rate
+        ]
+
+if __name__ == '__main__':
+    wpilib.run(Robot)
